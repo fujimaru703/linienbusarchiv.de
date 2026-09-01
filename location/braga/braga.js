f590532e10118bbd2db24d9e1e4ceb177530fc79
fujimaru703 <23ej107@ms.dendai.ac.jp>
2026-09-01 22:17:35 +0900

    Update braga.js

--- location/braga/braga.js
+++ location/braga/braga.js
@@ -10,11 +10,11 @@ const GTFS_BASE = "./";
 // 末尾の / はあってもなくても動きます。
 // 例:
 // const BRAGA_WORKER_URL = "https://braga-vehicles.example.workers.dev/";
-const BRAGA_WORKER_URL = "https://REPLACE-ME.workers.dev/";
+const BRAGA_WORKER_URL = "https://misty-frost-9f0e.fujimaru703.workers.dev/";
 
 const UPDATE_INTERVAL = 15000;
 const CAMERA_STORAGE_KEY = "braga-map-camera-v2";
-const DEFAULT_ICON_URL = "icon/bus-pictogram-50.png";
+const DEFAULT_ICON_URL = "icon/default-bus.png";
 
 // GTFSにないAPI用line IDが今後必要になった場合だけ追加。
 // 例: ["2T", "22F"]
