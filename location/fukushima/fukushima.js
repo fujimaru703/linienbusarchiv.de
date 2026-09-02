4a9f7dd44699e36d9c4b8ff62f41d2a77c458825
fujimaru703 <23ej107@ms.dendai.ac.jp>
2026-09-02 13:05:05 +0900

    Update fukushima.js

--- location/fukushima/fukushima.js
+++ location/fukushima/fukushima.js
@@ -14,7 +14,7 @@
 
     // 非営業・回送車両用 GAS Web API
     const GAS_FALLBACK_URL =
-      "https://script.google.com/macros/s/AKfycbwTR2z6rfcNOMgHwcyzqtgN179Q3caqYEAN1-zwfVwVfZwZio5VIvXp46cqxaW3DlTR/exec";
+      "https://script.google.com/macros/s/AKfycbwy8EobHt6NgBRJIwi7_DwSFLUpGacDR7gwLTdFP_u0F6fZ7mm1SO2v8Lehxydqq67x/exec";
 
     const CLOUDFLARE_UPDATE_INTERVAL = 5000;
     const FALLBACK_UPDATE_INTERVAL = 15000;
